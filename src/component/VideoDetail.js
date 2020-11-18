058814c7cdc69f2cfc4355e5c6c86b4f2398df6e
juker <juker1141@gmail.com>
2020-11-18 17:18:17 +0800

    update

---
+++ src/component/VideoDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+  
+  return (
+    <div>
+      <div>
+      { video.snippet.title }
+      </div>
+    </div>
+  );
+}
+
+export default VideoDetail;
